79a3a61d876d44f59ab5704111dd5a6a4d659d8f
VanilaMint <minhhh36@gmail.com>
2023-04-28 08:43:32 +0700

    a

---
+++ Lab2/AimsProject/src/Cart.java
@@ -0,0 +1,37 @@
+import java.nio.channels.Pipe;
+
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERS=20;
+    private DigitalVideoDisc itemOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERS];
+    private int qtyOrdered = 0;
+    public String addDigitalVideoDisc(DigitalVideoDisc disc){
+        if(qtyOrdered<20){
+            qtyOrdered++;
+            itemOrdered[qtyOrdered-1]=disc;
+            if(qtyOrdered==20)return "Your cart is now full";
+            else return "A disc has been added";
+        }
+        return "Your cart is already full";
+    }
+    public String removeDigitalVideoDisc(DigitalVideoDisc disc){
+        for(int i=0;i<qtyOrdered;i++){
+            if(itemOrdered[i] == disc){
+                for(int j=i+1;j<qtyOrdered;j++){
+                    DigitalVideoDisc tmp=itemOrdered[i-1];
+                    itemOrdered[i-1]=itemOrdered[i];
+                    itemOrdered[i]=tmp;
+                }
+                qtyOrdered--;
+                return "A disc has been removed";
+            }
+        }
+        return "That disc is not in your cart";
+    }
+    public float totalCost(){
+        float total=0;
+        for(int i=0;i<qtyOrdered;i++){
+            total+=itemOrdered[i].getCost();
+        }
+        return total;
+    }
+}
---
+++ Lab2/AimsProject/src/DigitalVideoDisc.java
@@ -0,0 +1,46 @@
+public class DigitalVideoDisc {
+    private String titles;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    public String getTitles() {
+        return titles;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public String getDirector() {
+        return director;
+    }
+    public int getLength() {
+        return length;
+    }
+    public float getCost() {
+        return cost;
+    }
+    public DigitalVideoDisc(String titles) {
+        this.titles = titles;
+    }
+    public DigitalVideoDisc(String titles, String category, float cost) {
+        this.titles = titles;
+        this.category = category;
+        this.cost = cost;
+    }
+    public DigitalVideoDisc(String titles, String category, String director, int length, float cost) {
+        this.titles = titles;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+    public DigitalVideoDisc(String titles, String category, String director, float cost) {
+        this.titles = titles;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+    }
+    
+}
+    
+    
