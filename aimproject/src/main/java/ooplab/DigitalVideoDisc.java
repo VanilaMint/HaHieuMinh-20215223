d006aa779ebbf4c50d44572f758cb0cba3b3ecc1
VanilaMint <minhhh36@gmail.com>
2023-05-15 20:35:35 +0700

    a

--- aimproject/src/main/java/ooplab/DigitalVideoDisc.java
+++ aimproject/src/main/java/ooplab/DigitalVideoDisc.java
@@ -1,12 +1,14 @@
 package ooplab;
 public class DigitalVideoDisc {
-    private String titles;
+    private static int nbDigitalVideoDiscs = 0;
+    private int id;
+    private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
-    public String getTitles() {
-        return titles;
+    public String getTitle() {
+        return title;
     }
     public String getCategory() {
         return category;
@@ -20,23 +22,31 @@ public class DigitalVideoDisc {
     public float getCost() {
         return cost;
     }
+    private void updateid(){
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
     public DigitalVideoDisc(String titles) {
-        this.titles = titles;
+        updateid();
+        this.title = titles;
     }
     public DigitalVideoDisc(String titles, String category, float cost) {
-        this.titles = titles;
+        updateid();
+        this.title = titles;
         this.category = category;
         this.cost = cost;
     }
     public DigitalVideoDisc(String titles, String category, String director, int length, float cost) {
-        this.titles = titles;
+        updateid();
+        this.title = titles;
         this.category = category;
         this.director = director;
         this.length = length;
         this.cost = cost;
     }
     public DigitalVideoDisc(String titles, String category, String director, float cost) {
-        this.titles = titles;
+        updateid();
+        this.title = titles;
         this.category = category;
         this.director = director;
         this.cost = cost;
